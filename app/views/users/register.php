ed2418d19b1c8fd4591572c8fb68142583a0bcfa
alextibbin <alex-egert.tibbin@khk.ee>
2020-02-26 08:56:15 +0200

    Loodud registreerimisvormi vaade

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,3 +1,33 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-register
+<!-- register form -->
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card card-body bg-light mt-5">
+            <h2>Create an Account</h2>
+            <form action="<?php echo URLROOT;?>/users/register" method="post">
+                <div class="form-group">
+                    <label for="name">Name: <sup>*</sup></label>
+                    <input type="text" id="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email: <sup>*</sup></label>
+                    <input type="text" id="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password: <sup>*</sup></label>
+                    <input type="password" id="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password2">Confirm Password: <sup>*</sup></label>
+                    <input type="password" id="password2" class="form-control">
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <input type="submit" value="Register" class="btn btn-block btn-info ">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <?php require_once APPROOT.'/views/inc/footer.php';?>
