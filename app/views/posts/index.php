6ab6e82544959f3389eec2b15fb31170a3fb9b51
alextibbin <alex-egert.tibbin@khk.ee>
2020-03-11 09:48:53 +0200

    Commititud blogi postituste lisamine

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,4 +1,8 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
+<?php flashShow('post_message');?>
+<?php if(isset($_SESSION['user_id'])) :?>
+    <a href="<?php echo URLROOT?>/posts/add" class="btn btn-dark">Add Post</a>
+<?php endif;?>
     <div class="row mb-3">
         <div class="col">
             <h1>Posts</h1>
